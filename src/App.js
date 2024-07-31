16c1cadfbf667bb794c01dbcfe61c3dbb2c451f9
vin007t <157238344+vin007t@users.noreply.github.com>
2024-07-31 22:23:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import HomePage from './HomePage';
+
+function App() {
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    );
+}
+
+export default App;
